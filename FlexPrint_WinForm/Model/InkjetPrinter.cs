a19e1c4cbc2ef143ddf6e1c5d884b612ac60f391
BlackBear <fanat991@ukr.net>
2024-04-15 09:31:55 +0300

    Add project files.

---
+++ FlexPrint_WinForm/Model/InkjetPrinter.cs
@@ -0,0 +1,29 @@
+﻿using FlexPrint_WinForm.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlexPrint_Console.Model
+{
+	public class InkjetPrinter : Printer
+	{
+		public bool Duplex { get; set; }
+
+		public override decimal CalculatePurchaseCost()
+		{
+			
+			//Console.WriteLine("Congratulations, you have received a cartridge as a gift!\n" +
+			//	$"{Price}$ due");
+			return base.CalculatePurchaseCost();
+		}
+
+		//public override string ToString()
+		//{
+		//	return $"InkjetPrinter Printer:\n Manufacturer: {Manufacturer}\n Model: " +
+		//		$" {Model}\n Product Code: {ProductCode}\n Price: {Price}\nPurpose:{Purpose}\n " +
+		//		$"PrinterSize: {PrinterSize}\n Duplex: {Duplex}\n";
+		//}
+	}
+}
---
+++ FlexPrint_WinForm/Model/LaserPrinter.cs
@@ -0,0 +1,38 @@
+﻿using FlexPrint_Console.Enum;
+using FlexPrint_WinForm.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlexPrint_Console.Model
+{
+	public class LaserPrinter : Printer
+	{
+
+		public LaserPrinterType LaserType { get; set; }
+
+		public override decimal CalculatePurchaseCost()
+		{
+			decimal discountedPrice = base.CalculatePurchaseCost();
+			if (Price > 5000)
+			{
+				
+				discountedPrice *= 0.95m;
+     //           Console.WriteLine("After buying a laser printer from 5000 5% discount\n" +
+					//$"{discountedPrice}$ due");
+				
+            }
+			
+			return discountedPrice;
+		}
+		//public override string ToString()
+		//{
+		//	return $"Laser Printer:\n Manufacturer: {Manufacturer}\n Model: " +
+		//		$" {Model}\n Product Code: {ProductCode}\n Price: {Price}$\n Purpose:{Purpose}\n " +
+		//		$"PrinterSize: {PrinterSize}\n LaserType: {LaserType}\n";
+		//}
+	}
+
+}
